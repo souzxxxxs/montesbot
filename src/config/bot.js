00fe8c47169ed9d8eaecf798d9272fe4eb7c63d1
souzxxxxs <ryymontes@gmail.com>
2026-09-10 21:43:17 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "I LOVE ERIN", 
         state: "I LOVE ERIN",     
         type: 2,               
       },
